4f82b2e2258bd60ad0903a334dfce7be76addeb0
KEUMIN <mskeum93@naver.com>
2025-11-18 22:02:21 +0900

    chore: kustomizaion 내 누락 속성 추가

--- gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
+++ gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-// Confirmation commit to trigger deployment : 2025-11-18 21:31
+// Confirmation commit to trigger deployment : 2025-11-18 22:01
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
