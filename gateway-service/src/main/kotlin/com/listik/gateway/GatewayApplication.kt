06520704cbfcb3d613eb6a538da805710e30ccef
KEUMIN <mskeum93@naver.com>
2025-11-18 18:07:41 +0900

    chore: redis 주소 에러 수정 (1)

--- gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
+++ gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-// Confirmation commit to trigger deployment : 2025-11-18 17:51
+// Confirmation commit to trigger deployment : 2025-11-18 18:07
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
