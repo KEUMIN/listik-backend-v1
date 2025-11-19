426e22d5116fbd683d5b30726f38d697c4cda95f
KEUMIN <mskeum93@naver.com>
2025-11-19 14:56:25 +0900

    chore: 디버깅용 레디스 관련 로그 레벨 제거

--- gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
+++ gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-// Confirmation commit to trigger deployment : 2025-11-19 14:48
+// Confirmation commit to trigger deployment : 2025-11-19 14:55
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
