35050f00e6029a0aa4d65e5649bb9b075cbf8d4e
KEUMIN <mskeum93@naver.com>
2025-11-19 14:50:20 +0900

    chore: startupProbe 누락 추가

--- gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
+++ gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-// Confirmation commit to trigger deployment : 2025-11-19 13:42
+// Confirmation commit to trigger deployment : 2025-11-19 14:48
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
