f77a61a31a99096e57998a157e2f219fc6515b5f
KEUMIN <mskeum93@naver.com>
2025-11-20 13:54:18 +0900

    chore: secret 변경

--- gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
+++ gateway-service/src/main/kotlin/com/listik/gateway/GatewayApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-// Confirmation commit to trigger deployment : 2025-11-20 13:07
+// Confirmation commit to trigger deployment : 2025-11-20 13:53
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
 }
